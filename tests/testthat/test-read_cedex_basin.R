c6c4091637c831428213ec241d69d797e90981c9
Roberto Molowny-Horas <roberto@creaf.uab.es>
2024-11-04 11:41:52 +0100

    Remove deleted files

--- tests/testthat/test-read_cedex_basin.R
+++
@@ -1,8 +0,0 @@
-test_that("Read cedex basin data", {
-  
-  expect_error(read_cedex_basin(cs = "ws84"))
-  expect_error(read_cedex_basin(basin = c("ebro", "aaa", "duero", "bbb")))
-  expect_error(read_cedex_basin(table = "aaa"))
-  
-  
-})
