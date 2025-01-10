9f8f74dacfc2e0ba872d464f37bc8e11a2d1ed31
Roberto Molowny-Horas <roberto@creaf.uab.es>
2025-01-10 14:56:08 +0100

    Delete hydrospain.Rcheck directory

--- hydrospain.Rcheck/00_pkg_src/hydrospain/NEWS.md
+++
@@ -1,3 +0,0 @@
-# hydrospain 0.0.1
-
-* Initial CRAN submission.
--- hydrospain.Rcheck/tests/testthat/test-select_coordinates.R
+++
@@ -1,7 +0,0 @@
-test_that("multiplication works", {
-
-  expect_no_condition(select_coordinates())
-  df <- data.frame(file = "afliq", file_coords = "estaf", id_join = "indroea")
-  expect_identical(select_coordinates("afliq"), df)
-  
-})
